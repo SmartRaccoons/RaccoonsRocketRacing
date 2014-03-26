f25d51b13e092f287d14836971c03929640202ca
bambis <termilv@gmail.com>
2014-03-26 16:31:12 +0200

    switch to primius

---
+++ public/d/js/run.js
@@ -0,0 +1,22 @@
+var r = new App.Router({el: $('body')}),
+    primus;
+App.socket.send.on('all', function(){
+    primus.write(Array.prototype.slice.call(arguments));
+});
+var connect = function(address){
+    primus = Primus.connect(address);
+    primus.on('data', function(data){
+        App.socket.receive.trigger.apply(App.socket.receive, data);
+    });
+    primus.on('timeout', function(){});
+    primus.on('end', function(){});
+    primus.on('close', function(){});
+    primus.on('disconnection', function(){});
+    primus.on('open', function(){
+//      App.socket.receive.trigger.apply(App.socket.receive, ['connect']);
+    });
+    return primus;
+}
+connect('').on('timeout', function(){
+   connect(':9052');
+});
